4a9b1cc6d4deab6043d898653ff281469e2bb4c2
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2017-07-26 12:04:58 +0200

    Initial commit

---
+++ src/main/java/com/example/simplemaven/SimpleMavenApplication.java
@@ -0,0 +1,12 @@
+package com.example.simplemaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleMavenApplication.class, args);
+	}
+}
